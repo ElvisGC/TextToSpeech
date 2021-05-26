8ccb042fbbd35c71bb00fad9e885a2682a537a2c
ElvisGC <elarguti@gmail.comm>
2021-05-25 19:05:58 -0500

    first commit

---
+++ Models/Producto.cs
@@ -0,0 +1,9 @@
+namespace Productos.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+    }
+}
